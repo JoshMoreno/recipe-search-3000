1750def430a13dbf351263370e296ecf68bf965f
Josh Moreno <josh@joshmoreno.com>
2024-02-21 23:16:12 -0800

    ensure author emails get reused to make testing via the ui easier

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -14,9 +14,30 @@ class RecipeFactory extends Factory
         return [
             'name' => $this->faker->sentence(3),
             'description' => $this->faker->text(),
-            'author_email' => $this->faker->safeEmail(),
+            'author_email' => $this->generateEmail(),
             'created_at' => now(),
             'updated_at' => now(),
         ];
+    }
+
+    public function generateEmail(): string
+    {
+        return $this->faker->randomElement([
+            'gourmetGenius@example.com',
+            'spicySensation@example.com',
+            'sweetSavant@example.com',
+            'bakingBard@example.com',
+            'umamiUnicorn@example.com',
+            'flavorVoyager@example.com',
+            'pastryPioneer@example.com',
+            'sushiSculptor@example.com',
+            'coffeeConnoisseur@example.com',
+            'veganVirtuoso@example.com',
+            'cheeseChampion@example.com',
+            'grillGuru@example.com',
+            'pastaPatron@example.com',
+            'dessertDesigner@example.com',
+            'fusionFanatic@example.com',
+        ]);
     }
 }
